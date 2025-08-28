67228a620f535d665f80499862115ce5014d5bc4
vrc <vrivera@peersyst.com>
2025-08-28 18:59:11 +0200

    test

--- api/swap-metrics.ts
+++ api/swap-metrics.ts
@@ -1,4 +1,1 @@
-// This file directly re-exports your TypeScript handler
-// Vercel will compile and run this as a serverless function
-
-export { default } from '../packages/api/src/handler.js';
+export { default } from './src/handler.js';
